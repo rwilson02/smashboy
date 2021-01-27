5019ac075d41a7ebc972e3f6ae1db6a25948c64d
Rory <rwilson2@ltu.edu>
2021-01-27 15:39:53 -0500

    added scripts folder
    it's just a separate folder for scripts

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public float speed, jumpForce;
+    public GameObject you;
+    public Rigidbody rb;
+    public Collider legs;
+    public KeyCode jump, run;
+    public string moveAxis = "Horizontal";
+    private int jumpTimes = 2;
+    
+    void OnCollisionEnter()
+    {
+        jumpTimes = 2;
+    }
+
+    void Move()
+    {
+        int dash;
+        float axis = Input.GetAxis(moveAxis);
+        
+        if (Input.GetKey(run))
+        {
+            dash = 2;
+        }
+        else dash = 1;
+
+        you.transform.Translate(new Vector3(axis * speed * Time.deltaTime * dash, 0));
+
+        if (Input.GetKeyDown(jump) && jumpTimes > 0)
+        {
+            if(rb.velocity.y < 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+            }
+
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode.Impulse); 
+            jumpTimes -= 1;
+            Debug.Log(jumpTimes);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
